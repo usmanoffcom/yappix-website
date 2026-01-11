655b7e4cb1bee782828fe2fea8d800c581c4ecdb
renatusmanoff <239621397+usmanoffcom@users.noreply.github.com>
2026-01-11 17:48:46 +0300

    Add Yandex.Metrika counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,6 +120,26 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+        {/* Yandex.Metrika counter */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+              ym(95481194, 'init', {webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/95481194" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
